1fef464f8ab62911afd25873c478fddd9b6af9a3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-12 07:10:27 +0700

    WIP shooter gameplay camera controller integration

---
+++ Scripts/CMShooterGameplayCameraController.cs
@@ -0,0 +1,23 @@
+namespace MultiplayerARPG.Cinemachine
+{
+    public class CMShooterGameplayCameraController : CMGameplayCameraController, IShooterGameplayCameraController
+    {
+        public bool EnableAimAssist { get; set; }
+        public bool EnableAimAssistX { get; set; }
+        public bool EnableAimAssistY { get; set; }
+        public bool AimAssistPlayer { get; set; }
+        public bool AimAssistMonster { get; set; }
+        public bool AimAssistBuilding { get; set; }
+        public bool AimAssistHarvestable { get; set; }
+        public float AimAssistRadius { get; set; }
+        public float AimAssistXSpeed { get; set; }
+        public float AimAssistYSpeed { get; set; }
+        public float AimAssistMaxAngleFromFollowingTarget { get; set; }
+        public float CameraRotationSpeedScale { get; set; }
+
+        public void Recoil(float x, float y)
+        {
+
+        }
+    }
+}
